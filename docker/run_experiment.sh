78c133a497895080c959e2aabd9fd90e80c48def
Lukas Pirch <lukas.pirch@tu-berlin.de>
2025-01-22 13:26:33 +0100

    migrate image to cuda

--- docker/run_experiment.sh
+++ docker/run_experiment.sh
@@ -9,6 +9,15 @@ fi
 
 seed=$1
 
+if [ ! -d ".venv" ]; then
+	echo "Creating virtual environment"
+	virtualenv .venv
+	source .venv/bin/activate
+	pip install -r requirements.txt
+fi
+
+source .venv/bin/activate
+
 in_train=data/raw/primevul_train.jsonl
 in_valid=data/raw/primevul_valid.jsonl
 in_test=data/raw/primevul_test.jsonl
@@ -20,5 +29,8 @@ touch $log_file
 python code/run.py --output_dir=./saved_models/regcn_l2_hs128_uni_ws5_lr5e4 --model_type=roberta --tokenizer_name=microsoft/graphcodebert-base --model_name_or_path=microsoft/graphcodebert-base \
 	--do_eval --do_test --do_train --train_data_file=$in_train --eval_data_file=$in_valid --test_data_file=$in_test \
 	--block_size 400 --train_batch_size 128 --eval_batch_size 128 --max_grad_norm 1.0 --evaluate_during_training \
-	--gnn ReGCN --learning_rate 5e-4 --epoch 100 --hidden_size 128 --num_GNN_layers 2 --format uni --window_size 5 \
-	--seed $seed  --pred_csv=$out_dir/predictions.csv 2>&1 | tee $log_file
+	--gnn ReGCN --learning_rate 5e-4 --epoch 10 --hidden_size 128 --num_GNN_layers 2 --format uni --window_size 5 \
+	--seed $seed  --pred_csv=$out_dir/predictions.csv --num_train_epochs 10 \
+	--training_percent 0.01 --seed $seed 2>&1 | tee $log_file
+
+deactivate
